3db1e13bf7827a6b71d665d3598ec3d8c9859cf9
AlexBulavin <bulavin1970@icloud.com>
2023-10-29 12:44:15 +0300

    3 script

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import "fmt" //Импортируем пакет fmt - format
 
 func main() {
-	var hello = "Hello world"
+	var hello string
+	fmt.Print("hello = ")
 	fmt.Println(hello)
 
-	outString := fmt.Sprintf("our string: %T", hello)
+	hello = "Hello world"
+	fmt.Println(hello)
+
+	outString := fmt.Sprintf("our string: %T", hello) //Вывели тип переменной hello
 	fmt.Println(outString)
 	fmt.Println()
 }
